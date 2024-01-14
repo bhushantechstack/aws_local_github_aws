8065898c74b2d3ab31466077cc151716f6a68a47
B G <BG>
2024-01-14 21:48:41 +0530

    Added bucket

--- lib/hello-stack.ts
+++ lib/hello-stack.ts
@@ -7,14 +7,8 @@ export class HelloStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-    // const s3Bucket = new aws_s3.Bucket(this, 'MyS3Bucket', {
-    //   bucketName:"my-first-bucket"
-    // });
+  
+    const s3Bucket = new aws_s3.Bucket(this, 'Bucket_for_storage', {
+    });
   }
 }
